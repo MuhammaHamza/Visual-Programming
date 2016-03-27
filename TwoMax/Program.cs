36a02cc1d2a9a3699495cca5b89484718f05dcb9
MuhammaHamza <h.usman1227@gmail.com>
2016-03-27 14:10:07 +0500

    Added files via upload

---
+++ TwoMax/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arra1 = { 1, 5, 4, 66, 76, 45, 33, 21 };
+            int temp;
+            int k = arra1.Length;
+            for (int j = 0; j <= arra1.Length; j++)
+            {
+                for (int i = 0; i <arra1.Length-1; i++)
+                {
+                    if (arra1[i] > arra1[i + 1])
+                    {
+                        temp = arra1[i+1];
+                        arra1[i+1] = arra1[i];
+                        arra1[i] = temp;
+
+                    }
+                    
+                }
+
+            }
+            for (int i = 0; i < arra1.Length; i++)
+            {
+                Console.WriteLine(arra1[i]);
+            }
+
+            Console.WriteLine("first Max" + arra1[k-1]);
+            Console.WriteLine("Seconed Max" + arra1[k-2]);
+            
+        }
+    }
+}
---
+++ removeDuplicateString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace removeDuplicateString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "helo olaf";
+            char[] arra = str.ToCharArray();
+            for (int i = 0; i < arra.Length; i++)
+            {
+                for (int j = 0; j <i; j++)
+                {
+                    if (arra[i] == arra[j])
+                    {
+                        arra[i] = '_'; 
+                    }
+                }
+                Console.WriteLine(arra[i]);
+            }
+            string s = new string(arra);
+            Console.WriteLine(s);
+        }
+    }
+}
